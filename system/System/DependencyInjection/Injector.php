89e4566d3bd85e9e9e5ee26ef89f4c5e0e3a2762
Dakarn <dakarn9@mail.ru>
2018-04-23 21:23:10 +0400

    test

---
+++ system/System/DependencyInjection/Injector.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: v.konovalov
+ * Date: 23.04.2018
+ * Time: 15:11
+ */
+
+namespace System\DependencyInjection;
+
+class Injector
+{
+    public function __construct()
+    {
+        //$reflexs = (new \ReflectionMethod($this->className,'__construct'))->getParameters();
+
+        //foreach($reflexs as $reflex) {
+        //    echo $reflex->getClass()->getName() .' - ' . $reflex->getName();
+        //}
+    }
+}
---
+++ system/System/DependencyInjection/InjectorInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: v.konovalov
+ * Date: 23.04.2018
+ * Time: 15:12
+ */
+
+namespace System\DependencyInjection;
+
+interface InjectorInterface
+{
+
+}
